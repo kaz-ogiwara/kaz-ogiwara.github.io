767553ada9d2f60b19d992079bf1aabc4421420c
Kazuki <kazuki.ogiwara@gmail.com>
2017-09-18 03:30:40 +0900

    Fixed population

--- viz/population/js/default.js
+++ viz/population/js/default.js
@@ -77,7 +77,6 @@ function init(){
 
 
  
-	// ジオメトリー
 	var i = 0;
   for(var y = 0; y < 13; y ++){
     for(var x = 0; x < 19; x ++){
@@ -108,14 +107,12 @@ function init(){
 		}
   }
 
-  // コントローラー
 	controls = new THREE.OrbitControls(camera, renderer.domElement);
 	controls.minDistance = 10;
 	controls.maxDistance = 500;
 	controls.maxPolarAngle = (Math.PI * 0.48);
 
-	// 自動回転
-//	controls.autoRotate = true;
+	controls.autoRotate = true;
 	controls.autoRotateSpeed = 2.0;
 
  	document.addEventListener('mousemove', onDocumentMouseMove, false);
