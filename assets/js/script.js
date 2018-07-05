f0825722c47ceff25d7000934445aeb49931760e
localmacuser <k-ogiwara@toyokeizai.co.jp>
2018-07-05 11:03:37 +0900

    Fix link directory bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,18 @@ $(function(){
       return 'click';
   })();
 
-  
+
   if ($("body").hasClass("home") || $("body").hasClass("works")) {
     $.getJSON("/kaz-ogiwara.github.io/assets/json/works.json", function(works){
       $.each(works, function(i, work) {
         if ($("body").hasClass("works") || i <= 2) {
           var html =  '<div class="work">'
-                      + '<div class="image" style="background-image:url(/kaz-ogiwara.github.io/assets/img/eyecatch_' + work.id + '.png)">'
-                        + '<a href="/kaz-ogiwara.github.io/works/' + work.id + '/description-en.html"></a>'
+                      + '<div class="image" style="background-image:url(assets/img/eyecatch_' + work.id + '.png)">'
+                        + '<a href="works/' + work.id + '/description-en.html"></a>'
                       + '</div>'
                       + '<div class="title">' + work.title + '</div>'
                     + '</div>';
-                    
+
           $("#works-block").append(html);
         }
       });
@@ -28,7 +28,7 @@ $(function(){
     //$("#menu-cover").fadeIn("fast");
     $("#menu").addClass("show");
   });
-  
+
   //$("#icon-close,#menu-cover").on("click touchstart", function(){
   $("#icon-close,#menu-cover").on(eClick, function(){
     //$("#menu-cover").fadeOut("fast");
