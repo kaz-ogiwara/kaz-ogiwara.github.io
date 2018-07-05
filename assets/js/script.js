23d40a06dddc3eabcc16e1d9bd9e387df3fd15f5
localmacuser <k-ogiwara@toyokeizai.co.jp>
2018-07-05 11:31:15 +0900

    Use full path

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 $(function(){
+  var ROOT = "https://kaz-ogiwara.github.io/";
+  if (location.hostname === "localhost") ROOT = "http://localhost:8888/kaz-ogiwara.github.io/";
+
   var eClick = (function() {
     if ('ontouchstart' in document.documentElement === true)
       return 'touchstart';
@@ -8,12 +11,12 @@ $(function(){
 
 
   if ($("body").hasClass("home") || $("body").hasClass("works")) {
-    $.getJSON("/kaz-ogiwara.github.io/assets/json/works.json", function(works){
+    $.getJSON(ROOT + "assets/json/works.json", function(works){
       $.each(works, function(i, work) {
         if ($("body").hasClass("works") || i <= 2) {
           var html =  '<div class="work">'
-                      + '<div class="image" style="background-image:url(assets/img/eyecatch_' + work.id + '.png)">'
-                        + '<a href="works/' + work.id + '/description-en.html"></a>'
+                      + '<div class="image" style="background-image:url(' + ROOT + 'assets/img/eyecatch_' + work.id + '.png)">'
+                        + '<a href="' + ROOT + 'works/' + work.id + '/description-en.html"></a>'
                       + '</div>'
                       + '<div class="title">' + work.title + '</div>'
                     + '</div>';
