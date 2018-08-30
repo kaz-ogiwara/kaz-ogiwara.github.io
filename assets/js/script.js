8bc383d0a0ff2c402233b606e7a72f20fd21fc24
localmacuser <k-ogiwara@toyokeizai.co.jp>
2018-08-30 13:35:16 +0900

    Implement pjax link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+function hideCover(){
+  if ($("#cover").is(":visible")) {
+    $("#cover").fadeOut("slow");
+  }
+}
+
+
 $(function(){
   var ROOT = "https://kaz-ogiwara.github.io/";
   if (location.hostname === "localhost") ROOT = "http://localhost:8888/kaz-ogiwara.github.io/";
@@ -9,22 +16,27 @@ $(function(){
       return 'click';
   })();
 
-/*
-  if ($("#pjax-content").hasClass("home") || $("#pjax-content").hasClass("works")) {
-    $.getJSON(ROOT + "assets/json/works.json", function(works){
-      $.each(works, function(i, work) {
-        if ($("#pjax-content").hasClass("works") || i <= 2) {
-          var html =  '<div class="work">'
-                      + '<div class="image" style="background-image:url(' + ROOT + 'assets/img/eyecatch_' + work.id + '.png)">'
-                        + '<a href="' + ROOT + 'works/' + work.id + '/description-en.html"></a>'
-                      + '</div>'
-                      + '<div class="title">' + work.title + '</div>'
-                    + '</div>';
+  hideCover();
 
-          $("#works-block").append(html);
-        }
+  // When pjax pagenation was ended
+  $(document).on('pjax:end', function() {
+    hideCover();
+  });
+
+  // When link with pjax was clicked
+  $(document).on(eClick, "a.pjax", function(e){
+    e.preventDefault();
+    var href = $(this).attr("href");
+
+    $.when(
+      $("#cover").fadeIn("fast"),
+    ).done(function() {
+      $.pjax({
+        url: href,
+        container: "#pjax-content",
+        fragment: "#pjax-content",
+        timeout: 5000
       });
     });
-  }
-*/
+  });
 });
